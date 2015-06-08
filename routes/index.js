500f0604907f9fae03192d7c25dedd9a370c6e03
gregthebusker <gregthebusker@gmail.com>
2015-06-08 16:47:56 -0400

    Add search page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/guides', function(req, res, next) {
+  res.render('guides');
 });
 
 module.exports = router;
