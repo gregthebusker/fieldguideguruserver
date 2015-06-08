faee26379906132fba963f6fc191b9ed530d1ad3
gregthebusker <gregthebusker@gmail.com>
2015-06-08 17:37:38 -0400

    Added missing

---
+++ react/parsekeys.js
@@ -0,0 +1,7 @@
+var keys = {
+  "appId": "iGJkfcqcNFcg2r537QG49nZzL3WhDuSNMm6KgsQM",
+  "jsKey": "nKYTEfMzToWqiM7B8lt54DkbESAhj44taQuacmWm",
+  "masterKey": "pbFyvrIFZEPE6LKk7UQRoCEyxx6sho2al3E82Z5n"
+};
+
+module.exports = keys;
---
+++ react/searchpage.jsx
@@ -0,0 +1,83 @@
+var React = require('react');
+var ThemeManager = require('material-ui/lib/styles/theme-manager')();
+var Colors = require('material-ui/lib/styles/colors');
+var Paper = require('material-ui/lib/paper');
+var Parse = require('parse').Parse;
+var parseKeys = require('./parsekeys.js');
+
+Parse.initialize(parseKeys.appId, parseKeys.jsKey);
+
+mixpanel.track('Search Results');
+
+function fetchFieldGuides(cb) {
+  var FieldGuide = Parse.Object.extend('fieldguide');
+  var query = new Parse.Query(FieldGuide);
+  query.find({
+    success: cb,
+    error: function(error) {
+      console.log(error);
+    }
+  });
+}
+
+var ResultList = React.createClass({
+  render: function() {
+    var items = this.props.items;
+    var papers;
+    if (items) {
+      papers = items.map(function(item) {
+        return (
+          <Paper key={item.get('ISBN')}>
+            <p>{item.get('ISBN')}</p>
+          </Paper>
+        );
+      });
+    } else {
+      papers = <h3>No Results</h3>;
+    }
+
+    return (
+      <div>
+        <h2>{this.props.title}</h2>
+        {papers}
+      </div>
+    );
+  }
+});
+
+var Search = React.createClass({
+  childContextTypes: {
+    muiTheme: React.PropTypes.object
+  },
+
+  getChildContext: function() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
+  },
+
+  componentWillMount: function() {
+    fetchFieldGuides(function(results) {
+      this.setState({
+        results: results
+      });
+    }.bind(this));
+  },
+
+  getInitialState: function() {
+    return {
+      results: undefined
+    };
+  },
+
+  render: function() {
+    return (
+      <ResultList
+        title="Books"
+        items={this.state.results}
+      />
+    );
+  }
+});
+
+module.exports = Search;
