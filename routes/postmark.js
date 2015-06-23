78b132555649521ee37598ac779868f0266a9147
gregthebusker <gregthebusker@gmail.com>
2015-06-23 12:28:34 -0400

    Handle all

--- routes/postmark.js
+++ routes/postmark.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
+router.all('/', function(req, res) {
   console.log(req.body);
 
   res.status(200);
