972e6fe3d0683a0b7de76b587aed2c5a3b1f4e93
gregthebusker <gregthebusker@gmail.com>
2015-06-24 11:11:00 -0400

    Scrap emails

--- routes/postmark.js
+++ routes/postmark.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
+router.use(bodyParser.json());
 router.all('/', function(req, res) {
-  console.log(req.body);
+  var text = req.body.TextBody;
+
+
+  var ScrapedEmail = Parse.Object.extend("scrapedemail");
+  var emails = [];
+
+  var matches = text.match(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/gi);
+  matches.foreach((str) => {
+    email = new ScrapedEmail();
+    email.set({
+      email: str
+    });
+  });
+
+  Parse.saveAll(emails);
 
   res.status(200);
   res.send('success');
