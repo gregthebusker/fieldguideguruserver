d7ae916e3703ff701778a218a63a9bda9adc9888
gregthebusker <gregthebusker@gmail.com>
2015-06-25 12:13:52 -0400

    Test

--- routes/postmark.js
+++ routes/postmark.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-//var Parse = require('parse').Parse;
+var Parse = require('parse').Parse;
 
-//Parse.initialize("iGJkfcqcNFcg2r537QG49nZzL3WhDuSNMm6KgsQM", "nKYTEfMzToWqiM7B8lt54DkbESAhj44taQuacmWm");
+Parse.initialize("iGJkfcqcNFcg2r537QG49nZzL3WhDuSNMm6KgsQM", "nKYTEfMzToWqiM7B8lt54DkbESAhj44taQuacmWm");
 
 
 router.use(bodyParser.json());
@@ -11,17 +11,17 @@ router.all('/', function(req, res) {
   var text = req.body.TextBody;
 
 
-//  var ScrapedEmail = Parse.Object.extend("scrapedemail");
+  var ScrapedEmail = Parse.Object.extend("scrapedemail");
 
-//  var matches = text.match(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/gi);
-  matches.foreach((str) => {
-//    email = new ScrapedEmail();
-//    email.set({
-//      email: str
-//    });
+  var matches = text.match(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/gi);
+  matches.foreach(function(str) => {
+    email = new ScrapedEmail();
+    email.set({
+      email: str
+    });
   });
 
-//  Parse.saveAll(emails);
+  Parse.saveAll(emails);
 
   res.status(200);
   res.send('success');
