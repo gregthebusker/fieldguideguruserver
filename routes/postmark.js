a83ac78b4f73006d4467731c886ba20fa5c72b02
gregthebusker <gregthebusker@gmail.com>
2015-06-25 12:11:11 -0400

    Test

--- routes/postmark.js
+++ routes/postmark.js
@@ -10,19 +10,19 @@ router.use(bodyParser.json());
 router.all('/', function(req, res) {
   var text = req.body.TextBody;
 
-/*
-  var ScrapedEmail = Parse.Object.extend("scrapedemail");
+
+//  var ScrapedEmail = Parse.Object.extend("scrapedemail");
 
   var matches = text.match(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/gi);
   matches.foreach((str) => {
-    email = new ScrapedEmail();
-    email.set({
-      email: str
-    });
+//    email = new ScrapedEmail();
+//    email.set({
+//      email: str
+//    });
   });
 
-  Parse.saveAll(emails);
-*/
+//  Parse.saveAll(emails);
+
   res.status(200);
   res.send('success');
 });
