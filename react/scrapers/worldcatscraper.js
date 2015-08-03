89ce54992b8321e8776c880864e65b3914bc32a8
gregthebusker <gregthebusker@gmail.com>
2015-08-02 22:07:03 -0400

    Fix limiter

--- react/scrapers/worldcatscraper.js
+++ react/scrapers/worldcatscraper.js
@@ -1,23 +1,19 @@
 var callOnEach = require('./utility.js').callOnEach;
 var saveImage = require('./utility.js').saveImage;
+var parseLimiter = require('./utility.js').parseLimiter;
 var parseKeys = require('./parsekeys.js');
 var Parse = require('parse').Parse;
-var RateLimiter = require('limiter').RateLimiter;
 var request = require('request');
 var cheerio = require('cheerio');
 
 Parse.initialize(parseKeys.appId, parseKeys.jsKey, parseKeys.masterKey);
-
-var rateLimit = 1000 / 15;
-var wcLimiter = new RateLimiter(1, 1000 / 10);
-var parseLimiter = new RateLimiter(1, rateLimit); 
 
 function getPriceMap(data, cb) {
   data.prices = {};
   var buyingLinksBase = "http://www.worldcat.org/wcpa/servlet/org.oclc.lac.ui.buying.AjaxBuyingLinksServlet?serviceCommand=getBuyingLinks&oclcno=";
 
   var url = buyingLinksBase + data.oclcno;
-  wcLimiter.removeTokens(1, function() {
+  parseLimiter.removeTokens(1, function() {
     request(url, function(error, response, html) {
       var $ = cheerio.load(html);
       var sellers = [];
@@ -49,7 +45,7 @@ function parseWorldCatData(obj, cb) {
     return;
   }
 
-  wcLimiter.removeTokens(1, function() {
+  parseLimiter.removeTokens(1, function() {
     request(url, function(error, response, html) {
       var $ = cheerio.load(html);
       var data = {};
