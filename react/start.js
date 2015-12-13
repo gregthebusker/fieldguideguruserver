9262b53402c10ef8c287463ecb8109e613a5017e
Greg Schechter <diabolomaster@gmail.com>
2015-12-12 19:36:39 -0800

    Take out typeahead

--- react/start.js
+++ react/start.js
@@ -49,10 +49,13 @@ var Start = React.createClass({
               <MainIcon />
               Where do you need a field guide to?
             </h3>
-            <LocationTypeahead
-              autoFocus={true}
-              onSelect={this.onSelectLocation}
-            />
+            {/*
+                <LocationTypeahead
+                  autoFocus={true}
+                  onSelect={this.onSelectLocation}
+                />
+            */}
+
           </div>
         </div>
       </div>
